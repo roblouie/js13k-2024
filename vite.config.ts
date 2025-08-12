ead02f8944e0f8d2c7f0a220262299377aff5c84
Rob Louie <rob.j.louie@gmail.com>
2025-08-12 16:49:59 -0400

    Mobile controls working

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, HmrContext, IndexHtmlTransformContext, Plugin } from 'vite';
+import { defineConfig, IndexHtmlTransformContext, Plugin } from 'vite';
 import path from 'path';
 import fs from 'fs/promises';
 import typescriptPlugin from '@rollup/plugin-typescript';
@@ -6,20 +6,10 @@ import { OutputAsset, OutputChunk } from 'rollup';
 import { Input, InputAction, InputType, Packer } from 'roadroller';
 import CleanCSS from 'clean-css';
 import { statSync } from 'fs';
-const { execFileSync } = require('child_process');
 import ect from 'ect-bin';
-import { exec } from 'node:child_process';
-
-const htmlMinify = require('html-minifier');
-const tmp = require('tmp');
-const ClosureCompiler = require('google-closure-compiler').compiler;
-
-const shaderMinifyConfig = {
-  shouldMinify: true,
-  shaderDirectory: './src/engine/shaders',
-  output: './src/engine/shaders/shaders.ts',
-  debounce: 2000,
-};
+import {defaultTerserOptions} from "./terser.config";
+import {execFileSync} from "child_process";
+import htmlMinify from "html-minifier";
 
 export default defineConfig(({ command, mode }) => {
   const config = {
@@ -31,7 +21,7 @@ export default defineConfig(({ command, mode }) => {
         '@': path.resolve(__dirname, './src'),
       }
     },
-    plugins: [minifyShaders(shaderMinifyConfig)]
+    plugins: undefined
   };
 
   if (command === 'build') {
@@ -41,8 +31,8 @@ export default defineConfig(({ command, mode }) => {
     config.base = '';
     // @ts-ignore
     config.build = {
-      minify: false,
-      target: 'es2020',
+      minify: 'terser',
+      target: 'es2022',
       modulePreload: { polyfill: false },
       assetsInlineLimit: 800,
       assetsDir: '',
@@ -52,59 +42,22 @@ export default defineConfig(({ command, mode }) => {
           manualChunks: undefined,
           assetFileNames: `[name].[ext]`
         },
-      }
+      },
+      terserOptions: defaultTerserOptions,
     };
     // @ts-ignore
-    config.plugins.push(typescriptPlugin(), closurePlugin(), roadrollerPlugin(), ectPlugin());
+    config.plugins = [typescriptPlugin(), roadrollerPlugin(), ectPlugin()];
   }
 
   return config;
 });
 
-function closurePlugin(): Plugin {
-  return {
-    name: 'closure-compiler',
-    // @ts-ignore
-    renderChunk: applyClosure,
-    enforce: 'post',
-  };
-}
-
-async function applyClosure(js: string, chunk: any) {
-  const tmpobj = tmp.fileSync();
-  // replace all consts with lets to save about 50-70 bytes
-  // ts-ignore
-  js = js.replaceAll('const ', 'let ');
-
-  await fs.writeFile(tmpobj.name, js);
-  const closureCompiler = new ClosureCompiler({
-    js: tmpobj.name,
-    externs: 'externs.js',
-    compilation_level: 'ADVANCED',
-    language_in: 'ECMASCRIPT_2020',
-    language_out: 'ECMASCRIPT_2020',
-  });
-  return new Promise((resolve, reject) => {
-    closureCompiler.run((_exitCode: string, stdOut: string, stdErr: string) => {
-      if (stdOut !== '') {
-        resolve({ code: stdOut });
-      } else if (stdErr !== '') { // only reject if stdout isn't generated
-        reject(stdErr);
-        return;
-      }
-
-      console.warn(stdErr); // If we make it here, there were warnings but no errors
-    });
-  });
-}
-
-
 function roadrollerPlugin(): Plugin {
   return {
     name: 'vite:roadroller',
     transformIndexHtml: {
-      enforce: 'post',
-      transform: async (html: string, ctx?: IndexHtmlTransformContext): Promise<string> => {
+      order: 'post',
+      handler: async (html: string, ctx?: IndexHtmlTransformContext): Promise<string> => {
         // Only use this plugin during build
         if (!ctx || !ctx.bundle) {
           return html;
@@ -127,7 +80,7 @@ function roadrollerPlugin(): Plugin {
           minifyCSS: true,
         };
 
-        const bundleOutputs = Object.values<(OutputAsset | OutputChunk)>(ctx.bundle);
+        const bundleOutputs = Object.values(ctx.bundle);
         const javascript = bundleOutputs.find((output) => output.fileName.endsWith('.js')) as OutputChunk;
         const css = bundleOutputs.find((output) => output.fileName.endsWith('.css')) as OutputAsset;
         const otherBundleOutputs = bundleOutputs.filter((output) => output !== javascript);
@@ -216,36 +169,4 @@ function ectPlugin(): Plugin {
       }
     },
   };
-}
-
-function minifyShaders(config: { shouldMinify: boolean, shaderDirectory: string, output: string, debounce: number }): Plugin {
-  function doMinification() {
-    debounce(async () => {
-      const filesInShaderDir = await fs.readdir(config.shaderDirectory);
-      const shaderFiles = filesInShaderDir.filter(file => file.endsWith('glsl'));
-      const fileArgs = shaderFiles.map(filename => {
-        return config.shaderDirectory.endsWith('/') ? `${config.shaderDirectory}${filename}` : `${config.shaderDirectory}/${filename}`;
-      }).join(' ');
-      const monoRunner = process.platform === 'win32' ? '' : 'mono ';
-      exec(`${monoRunner}shader_minifier.exe --format js ${fileArgs} -o ${config.output}`);
-    }, config.debounce);
-  }
-
-  return {
-    name: 'vite:shader-minify',
-    handleHotUpdate(context: HmrContext) {
-      if (!config.shouldMinify || !context.file.includes('glsl')) {
-        return;
-      }
-
-      doMinification();
-    }
-  };
-}
-
-let debounceTimeout: ReturnType<typeof setTimeout>;
-
-export function debounce(callback: (...args: any[]) => any, wait: number) {
-  clearTimeout(debounceTimeout);
-  debounceTimeout = setTimeout(callback, wait);
 }
